51103d6a0200597c07151716eb23db03d4133f47
wub <victorwoo@gmail.com>
2015-05-04 10:24:18 +0800

    Use ViewModel pattern.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,22 @@
-'use strict';
+(function () {
+  'use strict';
+  angular.module('rainbowApp')
+    .controller('MainCtrl', MainCtrl);
 
-angular.module('rainbowApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  function MainCtrl($scope, $http) {
+    var vm = this;
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    vm.awesomeThings = [];
 
-  });
+    activate();
+
+    ////////////////////////////////////////////
+
+    /*jshint validthis: true*/
+    function activate() {
+      $http.get('/api/things').success(function (awesomeThings) {
+        vm.awesomeThings = awesomeThings;
+      });
+    }
+  }
+}());
